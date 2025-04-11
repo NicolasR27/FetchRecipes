eedcb832cca0d0da0e1ecdba938f3a0c01ba58a8
Nicolas Rios <nickrios33@yahoo.com>
2025-04-11 13:07:15 -0700

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Nicolas Rios on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
